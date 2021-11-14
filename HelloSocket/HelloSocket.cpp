75259120c53d3507f1d991e154d3f334b96b3510
zombie <cwh675501560@gmail.com>
2021-11-14 17:24:25 +0800

    添加hellosocket.cpp

---
+++ HelloSocket/HelloSocket.cpp
@@ -0,0 +1,22 @@
+﻿//1 保证 winsocke2在windwos.h之前
+//2 WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <WinSock2.h>
+using namespace std;
+
+int main()
+{
+	//生成widows socket环境
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+
+
+
+	
+	WSACleanup();
+
+	return 0;
+}
